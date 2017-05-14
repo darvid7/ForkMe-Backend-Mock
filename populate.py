e6c17820315666acf2ce7b65f5391a208f0325de
darvid7 <bvdarvid@gmail.com>
2017-05-15 06:24:48 +1000

    Update Json sever for relevant info

--- populate.py
+++ populate.py
@@ -36,12 +36,37 @@ print('count repos returned: ' + str(len(repos)))
 json_data = []
 
 headers = {"Content-Type": "application/json"}
+
+# Only keep theses fields form Github API response.
+relvant_fields = [
+	"size", "full_name", "stargazers_count", "homepage", "description", "forks_count", "owner",
+	"id", "organizations_url", "avatar_url", "html_url", "url", "followers_url", "login", "type",
+	"repos_url", "gravatar_id", "created_at", "language", "open_issues_count", "score", "updated_at",
+	"has_wiki"
+]
+
 for repo in repos:
+	repo_dict = {}
 	print('repo name: ' + str(repo['full_name']), end=", ")
 	print('stars: ' + str(repo['stargazers_count']), end=", ")
 	print('watchers: ' + str(repo['watchers_count']), end='\n')
 	# Assume full name can be used to uniquely identify repositories (might need to change for implementation that looks back in time).
-	data = {repo['full_name']: repo}
+	for key, value in repo.items():
+		if key not in relvant_fields:  # Skip over unwanted fields.
+			continue
+		if key == "owner":
+			#  Parse owner field.
+			owner_dict = {}
+			for owner_key, owner_value in repo["owner"].items():
+				if owner_key not in relvant_fields:
+					continue
+				owner_dict[owner_key] = owner_value
+			repo_dict["owner"] = owner_dict
+		else:
+			# Add key.
+			repo_dict[key] = value
+
+	data = {repo['full_name']: repo_dict}
 	jsonify = json.dumps(data)
 	requests.post(json_server_endpoint, data=jsonify, headers=headers)
 	time.sleep(1)
